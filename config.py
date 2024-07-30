913558089f9381be9c5499994a08f6023d3371eb
FiverrYonis <yonisLacorte@proton.me>
2024-07-30 11:26:22 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+CAPTCHA_KEY = "c260a29cceb7a893c41974cf0c0958a3"
+BOT_TOKEN = "7305218336:AAG0UHnfoPVDglF8woS4Unr7E5smaavk0Rc"
+CHAT_ID = "-4202492708"
+MINUTES = 5
+
+SECONDS = MINUTES * 60
+
+
+CAPTCHA_TEXT = "captchaText"
+NO_APPOINTMENTS_TEXT = "Unfortunately, there are no appointments available at this time"
+BASE_URL = "https://service2.diplo.de/rktermin/"
